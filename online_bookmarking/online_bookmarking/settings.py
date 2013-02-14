53ed3a66821ad102d3c424804db16b84082e2a7b
sudan <sudan.s@inmobi.com>
2013-02-14 23:15:22 +0530

    Folder structure created and added some config in settings.py

--- online_bookmarking/online_bookmarking/settings.py
+++ online_bookmarking/online_bookmarking/settings.py
@@ -1,4 +1,6 @@
 # Django settings for online_bookmarking project.
+import os
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -60,7 +62,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_PATH,'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -110,6 +112,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH,"templates"),
 )
 
 INSTALLED_APPS = (
