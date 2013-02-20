e7176949afb18f5f7375f1c2dc3068219c65ba8c
Sumanth Prabhu <sumanthprabhu.104@gmail.com>
2013-02-20 18:20:20 +0530

    Home Page: Added bookmark specific retrieval methods and the view for home page

--- online_bookmarking/auth/views.py
+++ online_bookmarking/auth/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 
 def login(request):
     ''' Login form '''
     return render_to_response('login.html')
 
-def home(request):
-    ''' Home page '''
-    return render_to_response('home.html')
