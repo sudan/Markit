836250ed166f848dbb72ec988dfe6f7a07b678b8
sudan <sudan.s@inmobi.com>
2013-02-24 20:19:24 +0530

    added validation to usernme

--- online_bookmarking/auth/forms.py
+++ online_bookmarking/auth/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+import re
 
 class SignUpForm(forms.Form):
 	email = forms.EmailField(error_messages={'required':'Invalid Email'},widget=forms.widgets.TextInput(attrs={'placeholder':'Enter your email '}))
@@ -7,7 +8,14 @@ class SignUpForm(forms.Form):
 	last_name = forms.CharField(required=False,widget=forms.widgets.TextInput(attrs={'placeholder':'Last name'}))
 	password = forms.CharField(error_messages={'required':'Invalid Password'},widget=forms.widgets.PasswordInput(attrs={'placeholder':'Password'}))
 	password_confirmation = forms.CharField(error_messages={'required':'Invalid Password Confirmation'},required=True,widget=forms.widgets.PasswordInput(attrs={'placeholder':'Password Confirmation'}))
-	
+
+	def clean_username(self):
+		username = self.cleaned_data['username']
+		valid = re.match('^[\w]+$',username) is not None
+		if not valid:
+			raise forms.ValidationError('Username should contain only alpha numeric characters')
+		return username 
+
 class LoginForm(forms.Form):
 	email = forms.CharField(error_messages={'required':'Invalid Email'},widget=forms.widgets.TextInput(attrs={'placeholder':'Enter your email'}))
 	password = forms.CharField(error_messages={'required':'Invalid Password'},widget=forms.widgets.PasswordInput(attrs={'placeholder':'Password'}))
