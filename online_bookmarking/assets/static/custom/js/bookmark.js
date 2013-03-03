53a522fea1406cd897ab611a5370297ace8a93c6
sudan <sudan.s@inmobi.com>
2013-03-03 10:22:47 +0530

    removed handlebars

--- online_bookmarking/assets/static/custom/js/bookmark.js
+++ online_bookmarking/assets/static/custom/js/bookmark.js
@@ -1,1 +1,20 @@
-()
+(function($,window,document,undefined){
+
+	var Bookmark = Backbone.Model.extend({
+		defaults:{
+			url: '',
+			name: '',
+			description: '',
+			visibility: '',
+			created_date: '',
+			bookmark_id: ''
+		},
+		urlRoot: '/bookmark'
+	});
+
+	var Bookmarks = Backbone.Collection.extend({
+		model: Bookmark,
+		url: '/bookmarks'
+	});
+
+})(jQuery,window,document);
