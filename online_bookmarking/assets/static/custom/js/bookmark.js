7754c15c360ed96688e46fcdacdb9bfe5c88475d
sudan <sudan.s@inmobi.com>
2013-03-03 12:44:12 +0530

    fetch bookmarks from backbone.js implemened

--- online_bookmarking/assets/static/custom/js/bookmark.js
+++ online_bookmarking/assets/static/custom/js/bookmark.js
@@ -1,13 +1,14 @@
 (function($,window,document,undefined){
 
+	"use strict"
+
 	var Bookmark = Backbone.Model.extend({
 		defaults:{
 			url: '',
 			name: '',
 			description: '',
 			visibility: '',
-			created_date: '',
-			bookmark_id: ''
+			creation_date: ''
 		},
 		urlRoot: '/bookmark'
 	});
@@ -17,4 +18,56 @@
 		url: '/bookmarks'
 	});
 
-})(jQuery,window,document);
+	var BookmarkView = Backbone.View.extend({
+		tagName: 'article',
+		className: 'bookmark',
+		template: $('#bookmarkDisplayTemplate').html(),
+		
+		render: function(){
+			
+			var tmpl = _.template(this.template);
+			this.$el.html(tmpl(this.model.toJSON()));
+			return this;
+		}
+	});
+
+	var BookmarksView = Backbone.View.extend({
+		el: $('#bookmark_list'),
+
+		initialize: function(){
+			
+			var self = this;
+			this.collection = new Bookmarks();
+			this.collection.fetch({
+
+				success: function(){
+					self.render();
+				},
+				error: function(){
+
+				}
+
+			});
+		},
+
+		render: function(){
+
+			var self = this;
+			_.each(this.collection.models,function(bookmark){
+				self.renderBookmark(bookmark);
+			},this);
+		},
+
+		renderBookmark: function(bookmark){
+
+			var bookmarkView = new BookmarkView({
+				model: bookmark
+			});
+			this.$el.append(bookmarkView.render().el);
+
+		}
+	});
+
+	var bookmarks = new BookmarksView();
+
+})(jQuery,window,document)
