e60e2bbf56671ef550368a2a6b4908bb0fd70978
sudan <sudan.s@inmobi.com>
2013-03-10 23:23:27 +0530

    urls.py organized

--- online_bookmarking/online_bookmarking/urls.py
+++ online_bookmarking/online_bookmarking/urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 
+    #auth related urls
     url(r'^$','auth.views.index'),
     url(r'^signup/$','auth.signup.register'),
     url(r'^login/$','auth.signin.login'), 
@@ -21,23 +22,28 @@ urlpatterns = patterns('',
     url(r'^change_password/$','auth.change_password.change_password'),
     url(r'^edit_profile/$','auth.edit_profile.edit_profile'),
     
+    #bookmark related urls
     url(r'^home$', 'bookmark.bookmarks.display_bookmarks'),
     url(r'^bookmarks/$', 'bookmark.bookmarks.display_bookmarks'),
     url(r'^bookmark/$', 'bookmark.bookmarks.create_bookmark'),
     url(r'^delete_bookmark/$','bookmark.bookmarks.delete_bookmark'),
 
+    #category related urls
     url(r'^category/$','category.views.create_category'),
     url(r'^categories/$','category.views.get_categories'),
+    url(r'^add_bookmarks_to_category/$','category.views.add_bookmarks_to_category'),
     
+    #tag related urls
     url(r'^tag/$','tags.views.tag_bundle'),
     url(r'^tag_list/$','tags.views.retrieve_tags'),
-    url(r'^add_bookmarks_to_category/$','category.views.add_bookmarks_to_category'),
+    url(r'^tag_names/$','tags.views.retrieve_tags'),
+    url(r'^tags/(?P<tag_id>\d+)','tags.views.get_bookmarks_for_tags'),
+    
+    #socialize related urls
     url(r'^users/$','socialize.relationship.users'),
     url(r'^toggle/$','socialize.relationship.toggle_relationship'),
     url(r'^profile/(?P<profile_name>\w+)/$','socialize.profile.profile'),
     
-    
-    url(r'^tag_names/$','tags.views.retrieve_tags'),
-    url(r'^tags/(?P<tag_id>\d+)','tags.views.get_bookmarks_for_tags'),
+    #future implementation
     url(r'', include('social_auth.urls')),
 )
